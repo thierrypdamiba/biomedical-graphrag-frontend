28d54fcb062a654870667fd412799fdfc5b249f3
thierrypdamiba <thierrypdamiba@users.noreply.github.com>
2026-01-28 09:06:56 -0800

    Fix OpenGraph image alt text to PubMed Navigator

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import { ImageResponse } from "next/og";
 
 export const runtime = "edge";
-export const alt = "Biomedical GraphRAG";
+export const alt = "PubMed Navigator";
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
 
@@ -56,7 +56,7 @@ export default async function Image() {
             display: "flex",
           }}
         >
-          Biomedical GraphRAG
+          PubMed Navigator
         </div>
 
         {/* Subtitle */}
